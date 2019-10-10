8ecf1310bbc4831b964b5fc16017ae5dcf77c87c
rckmath <38138775+rckmath@users.noreply.github.com>
2019-10-10 19:20:48 -0300

    Developed a object oriented dynamic queue; doc code

---
+++ lib/dyn_queue.h
@@ -0,0 +1,70 @@
+// Fila dinamica
+
+#ifndef dyn_queue_h
+#define dyn_queue_h
+
+#include <iostream>
+
+class Queue {
+    struct node {
+        queue_element data;
+        struct node *next;
+    } * front, *rear, *temp;
+    typedef struct node node;
+
+public:
+    /**
+     * Construtor, inicializa a fila nula
+     */
+    Queue() {
+        front = NULL;
+        rear = NULL;
+    }
+
+    /**
+     * Insere no final da fila
+     */
+    void insert(queue_element x) {
+        temp = new node();
+        temp->data = x;
+
+        if (front == NULL) {
+            front = temp;
+            rear = temp;
+        } else {
+            temp->next = NULL;
+            rear->next = temp;
+            rear = temp;
+        }
+    }
+
+    /**
+     *  Elimina o elemento da primeira posicao da fila e retorna ele
+     */
+    queue_element del() {
+        temp = new node();
+        temp = front;
+        queue_element data = front->data;  // Recebe a info que estarei retirando da fila
+
+        if (front == NULL) {
+            std::cout << "Fila vazia!";
+            return NULL;
+        } else {
+            front = front->next;
+            delete temp;  // Limpa na memoria
+            return data;
+        }
+    }
+
+    /**
+     * Verifica se fila esta vazia, retorna 1 caso estiver
+     */
+    int is_empty() {
+        if (front == NULL) {
+            return 1;
+        }
+        return 0;
+    }
+};
+
+#endif
